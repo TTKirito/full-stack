fcf5e3c0afb89f0a8016cf83807b72858261e353
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-02 00:07:52 +0700

    54: fix authenticate gogle

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -3,7 +3,7 @@ import { gql } from "apollo-server-express";
 export const typeDefs = gql`
   type Viewer {
     id: ID
-    toke: String
+    token: String
     avatar: String
     hasWallet: Boolean
     didRequest: Boolean!
