463c47430a1ad9e78c23e82aa628d7a5f898af91
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-01 22:21:53 +0700

    47.  database collection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { Database, Listing } from "../lib/type";
+import { Booking, Database, Listing, User } from "../lib/type";
 
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}.mongodb.net/`;
 
@@ -9,6 +9,9 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main");
 
   return {
-    listings: db.collection<Listing>("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
+ 
--- src/lib/type.ts
+++ src/lib/type.ts
@@ -1,20 +1,23 @@
 import { Collection, ObjectId } from "mongodb";
 
+export interface Booking {
+  _id: ObjectId;
+}
+
 export interface Listing {
   _id: ObjectId;
-  title: string;
-  image: string;
-  address: string;
-  price: number;
-  numOfGuests: number;
-  numOfBeds: number;
-  rating: number;
-  numOfBaths: number;
+}
+
+export interface User {
+  _id: ObjectId
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>
 }
+
 
 // ---
 
