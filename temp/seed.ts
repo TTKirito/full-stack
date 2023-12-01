96c68cd06851b5e63ef247227f0822fa4fb46813
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-01 22:45:03 +0700

    49: update seed & clear database

--- temp/seed.ts
+++ temp/seed.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 import { ObjectId } from "mongodb";
 import { connectDatabase } from "../src/database";
-import { Listing } from "../src/lib/type";
+import { Listing, ListingType, User } from "../src/lib/type";
 
 const seed = async () => {
   try {
@@ -13,21 +13,43 @@ const seed = async () => {
       {
         _id: new ObjectId(),
         title: "cooper",
+        description: "Clean and fully furnished apartment",
         image:
           "https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340",
+        host: "5d378db94e84753160e08b57",
+        type: ListingType.Apartment,
         address: "170 Nguyen chi thanh, hai chau, da nang, viet nam",
-        price: 10000,
-        numOfGuests: 2,
-        numOfBeds: 1,
-        numOfBaths: 2,
-        rating: 5,
+        country: "Viet Nam",
+        admin: "Cooper",
+        city: "Da Nang",
+        bookings: [],
+        bokingsIndex: {},
+        price: 1234,
+        numOfGuests: 3,
+      },
+    ];
+
+    const users: User[] = [
+      {
+        _id: new ObjectId("5d378db94e84753160e08b55"),
+        token: "token_*************",
+        name: "Thuan",
+        avatar:
+          "https://media.macphun.com/img/uploads/customer/how-to/608/15542038745ca344e267fb80.28757312.jpg?q=85&w=1340",
+        contact: "thuanton98@gmail.com",
+        walletId: "acct_**********",
+        income: 723796,
+        bokings: [],
+        listing: [new ObjectId("5d378db94e84753160e08b31")],
       },
     ];
 
     for (const listing of listings) {
       await db.listings.insertOne(listing);
     }
-
+    for (const user of users) {
+      await db.users.insertOne(user);
+    }
     console.log("[seed]: success");
   } catch (err) {
     throw new Error("failed to seed database");
