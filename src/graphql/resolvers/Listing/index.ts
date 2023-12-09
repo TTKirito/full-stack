85c2da84a80a04a398d915df2edcfbb9f6b09b29
Thuận Tôn <137130109+ThuanTon98@users.noreply.github.com>
2023-12-09 07:29:22 +0700

    66: listing query

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,6 +1,11 @@
 import { Listing } from "../../../lib/type";
 
 export const listingResolvers = {
+  Query: {
+    listing: () => {
+      return "Query.listing";
+    },
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
